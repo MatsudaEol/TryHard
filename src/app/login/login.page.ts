3c2485b995954ded693008b9a1f76163c37eabff
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-09-24 13:21:48 -0300

    login-back-end

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
+  username: string = '';
+  password: string = '';
+  showAlert: boolean = false;
+  alertMessage: string = '';
   showPassword: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public alertController: AlertController
+  ) {}
 
-  ngOnInit() {
+  async login() {
+    if (this.username && this.password) {
+      if (this.username === 'vitor' && this.password === 'vitor123') {
+        this.router.navigate(['/tabs/home']);
+      } else {
+        const alert = await this.alertController.create({
+          message: 'Usuário ou senha incorretos.',
+          buttons: ['OK']
+        });
+
+        await alert.present();
+      }
+    } else {
+      const alert = await this.alertController.create({
+        message: 'Preencha os campos.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
   }
 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
-
 }
