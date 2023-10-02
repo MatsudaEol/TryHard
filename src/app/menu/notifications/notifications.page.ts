6018dad6a15202a2d4408ba6b34a62f72ad8def7
GabrielGodoy7 <142518644+GabrielGodoy7@users.noreply.github.com>
2023-10-02 20:35:49 -0300

    notifications inicio

--- src/app/menu/notifications/notifications.page.ts
+++ src/app/menu/notifications/notifications.page.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
-  templateUrl: './notifications.page.html',
-  styleUrls: ['./notifications.page.scss'],
+  templateUrl: 'notifications.page.html',
+  styleUrls: ['notifications.page.scss'], // Se você tiver um arquivo de estilo
 })
-export class NotificationsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NotificationsPage {
+  segment = 'novas'; // Certifique-se de ter a propriedade 'segment' no seu componente
 }
