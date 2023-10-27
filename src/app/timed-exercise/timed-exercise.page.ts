91f360948d6f9395bf22378fde6ea3714a9f7583
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-10-27 17:28:11 -0300

    timer

--- src/app/timed-exercise/timed-exercise.page.ts
+++ src/app/timed-exercise/timed-exercise.page.ts
@@ -10,7 +10,10 @@ import { ExerciseService } from '../services/exercise.service';
 })
 export class TimedExercisePage implements OnInit {
   exerciseData: any;
-  
+  displayTime: string = '00:00'; // Tempo inicial (minutos:segundos)
+  isRunning: boolean = false; // Estado do timer
+  timerInterval: any; // Referência para o setInterval
+
   constructor(private route: ActivatedRoute, private authService: AuthenticationService, private exerciseService: ExerciseService) { }
 
   ngOnInit() {
@@ -20,14 +23,42 @@ export class TimedExercisePage implements OnInit {
 
       if (exerciseId && userId) {
         this.exerciseService.getExerciseDetails(userId, exerciseId).subscribe((exerciseData) => {
-          //console.log('Detalhes do exercício:', exerciseData);
           this.exerciseData = exerciseData;
-
           // Nota:  Inserir um serviço de gerenciamento de estado
         });
       }
     });
-
   }
-  
+
+  startTimer() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      let totalSeconds = 0;
+
+      this.timerInterval = setInterval(() => {
+        totalSeconds++;
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+
+        this.displayTime = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
+      }, 1000);
+    }
+  }
+
+  pauseTimer() {
+    if (this.isRunning) {
+      this.isRunning = false;
+      clearInterval(this.timerInterval);
+    }
+  }
+
+  stopTimer() {
+    this.isRunning = false;
+    clearInterval(this.timerInterval);
+    this.displayTime = '00:00'; // Reinicie o tempo quando parar
+  }
+
+  padZero(num: number) {
+    return num.toString().padStart(2, '0');
+  }
 }
