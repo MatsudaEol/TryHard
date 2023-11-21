496096861014cea2d3e2b11637eb9e5d90c08096
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-11-21 19:24:39 -0300

    introdução

--- src/app/introduction/first-introduction/first-introduction.page.ts
+++ src/app/introduction/first-introduction/first-introduction.page.ts
@@ -11,24 +11,32 @@ export class FirstIntroductionPage {
     public alertButtons = ['OK'];
   public alertInputs = [
     {
-      placeholder: 'Insira seu gênero',
-    },
-    {
-      type: 'decimal',
-      placeholder: 'Peso',
-      min: 1,
-      max: 100,
-    },
-    {
-      type: 'number',
-      placeholder: 'Digite sua altura',
-      min: 1,
-      max: 100,
-    },
-    {
-      type: 'textarea',
-      placeholder: 'A little about yourself',
-    },
-  ];
+      name: 'gender',
+      type: 'select',
+      placeholder: 'Selecione seu gênero',
+      options: ['Masculino', 'Feminino'],
+  },
+  {
+    type: 'number',
+    placeholder: 'Peso (kg)',
+    min: 1,
+    max: 300,
+  },
+  {
+    type: 'text',
+    placeholder: 'Altura (cm)',
+    pattern: '^[0-9]{1,3}(,[0-9]{1,2})?$',
+    title: 'Informe a altura no formato correto (ex: 180,50)',
+},
+  {
+    name: 'training',
+    type: 'radio',
+    label: 'Você já treinou?',
+    options: [
+        { label: 'Sim', value: 'Sim' },
+        { label: 'Não', value: 'Não' }
+    ],
+}
+];
 }
 
