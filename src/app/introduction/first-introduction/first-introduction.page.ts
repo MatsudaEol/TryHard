7e0409ce13e046fd872ba0add1dcb55be076acd8
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-12-01 22:17:10 -0300

    intro-completed

--- src/app/introduction/first-introduction/first-introduction.page.ts
+++ src/app/introduction/first-introduction/first-introduction.page.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
-
+import { ModalController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-first-introduction',
   templateUrl: './first-introduction.page.html',
-  styleUrls: ['./first-introduction.page.scss'],
+  styleUrls: ['./first-introduction.page.scss']
 })
 export class FirstIntroductionPage {
+
+  constructor(public modalController: ModalController) {
+
+  }
   
+
 }
-
-
