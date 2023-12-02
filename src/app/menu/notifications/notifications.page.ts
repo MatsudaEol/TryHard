ecf5c5290dedbaf7c72bcf70943117a2bcca8ab9
Daniel Posebon <110265861+MatsudaEol@users.noreply.github.com>
2023-12-01 22:21:26 -0300

    Notificações

--- src/app/menu/notifications/notifications.page.ts
+++ src/app/menu/notifications/notifications.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-notifications',
@@ -8,7 +11,47 @@ import { Component } from '@angular/core';
 export class NotificationsPage {
   segment = 'novas';
   notifications: Notification[] = [];
+  welcome = true;
+  userData: any;
+  userId: string;
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth, private firestore: AngularFirestore, private userService: UserService) {
+    this.userData = {};
+  }
+
+  ngOnInit() {
+    this.afAuth.authState.subscribe(async (user) => {
+      if (user) {
+        this.userData = await this.userService.getUser(user.uid);
+        this.userId = user.uid;
+      }
+    });
+  }
+
+  onItemSwipe(event: any) {
+    console.log('Item deslizado!', event);
+    this.deleteNotification();
+  }
+
+  async deleteNotification() {
+    const notificationRef = this.firestore.collection('userNotifications').doc(this.userId);
+  
+    await notificationRef.update({ welcome: false });
+
+  }
+
+
+async checkAndSetWelcomeStatus() {
+  const notificationRef = this.firestore.collection('userNotifications').doc(this.userId);
+  const notificationDoc = await notificationRef.get().toPromise();
+
+  if (notificationDoc.exists) {
+    const notificationData = notificationDoc.data() as { welcome: boolean } | undefined;
+
+    if (notificationData && typeof notificationData.welcome === 'boolean') {
+      this.welcome = notificationData.welcome; // Define 'welcome' com o valor do Firestore
+    }
+  }
+}
 
 }
