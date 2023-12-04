b9ef676acde5eaa89a05027772d34db87bde3313
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-12-04 17:52:33 -0300

    Tela de exe repetição

--- src/app/repetitive-exercise/repetitive-exercise.page.ts
+++ src/app/repetitive-exercise/repetitive-exercise.page.ts
@@ -25,14 +25,16 @@ export class RepetitiveExercisePage implements OnInit {
   userId: string | null = null;
   isRunning: boolean = false;
   repetitionCount: number = 0;
-  displayTime: string = '00:00'; // Tempo inicial (minutos:segundos)
+  displayTime: string = '00:30'; // Tempo inicial (minutos:segundos)
   timerInterval: any; // Referência para o setInterval
   totalElapsedSeconds: number = 0; // Tempo total decorrido
   breakInterval: any;
   breakTimerActive: boolean = false;
   originalTimerColor = 'rgba(251, 251, 251, 0.992)';
   breakTimerColor = this.originalTimerColor;
-
+  breakTimeInSeconds: number = 30; // Tempo de descanso em segundos
+  breakDisplayTime: string = '00:30'; // Tempo inicial de descanso (minutos:segundos)
+  breakTimerInterval: any; // Referência para o setInterval do tempo de descanso
 
   constructor(
     private route: ActivatedRoute,
@@ -53,36 +55,14 @@ export class RepetitiveExercisePage implements OnInit {
       if (exerciseId && this.userId) {
         // Obtém os detalhes do exercício do serviço de exercícios
         this.exerciseService.getExerciseDetails(this.userId, exerciseId).subscribe((exerciseData) => {
+          // Adiciona uma propriedade para armazenar o índice da série atual
+          exerciseData.currentSerieIndex = 0;
           this.exerciseData = exerciseData;
         });
       }
     });
   }
 
-  startTimer() {
-    // Verifica se o timer não está em execução
-    if (!this.isRunning) {
-      this.isRunning = true;
-      if (this.totalElapsedSeconds === 0) {
-        this.totalElapsedSeconds = 900; //Daniel, PUXAR DO BANCO O TEMPO DO TREINO
-      }
-
-      // Inicia o intervalo para atualizar o tempo
-      this.timerInterval = setInterval(() => {
-        // Decrementa o tempo total decorrido
-        if (this.totalElapsedSeconds > 0) {
-          this.totalElapsedSeconds--;
-          const minutes = Math.floor(this.totalElapsedSeconds / 60);
-          const seconds = this.totalElapsedSeconds % 60;
-
-          // Atualiza o tempo exibido
-          this.displayTime = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
-        } else {
-          this.stopTimer(); // Quando o tempo chegar a zero, para o timer
-        }
-      }, 1000);
-    }
-  }
 
   pauseTimer() {
     if (this.isRunning) {
@@ -92,7 +72,6 @@ export class RepetitiveExercisePage implements OnInit {
     }
   }
 
-  // Para o timer
   stopTimer() {
     this.isRunning = false;
     clearInterval(this.timerInterval);
@@ -100,7 +79,6 @@ export class RepetitiveExercisePage implements OnInit {
     this.totalElapsedSeconds = 0; // Reseta o tempo total decorrido quando o timer é parado
   }
 
-  // Adiciona um zero à esquerda para números menores que 10
   padZero(num: number) {
     return num.toString().padStart(2, '0');
   }
@@ -108,20 +86,17 @@ export class RepetitiveExercisePage implements OnInit {
   startBreakTimer() {
     if (!this.breakTimerActive) {
       this.breakTimerActive = true;
-      const breakDuration = 60;
-      this.breakTimerActive = true;
-      this.breakTimerColor = 'rgba(99, 202, 99, 0.805' // Tempo de descanso em segundos (1 minuto neste exemplo)
-      let remainingBreakTime = breakDuration;
+      let remainingBreakTime = this.breakTimeInSeconds;
 
       // Inicia o intervalo para atualizar o tempo de descanso
-      this.breakInterval = setInterval(() => {
+      this.breakTimerInterval = setInterval(() => {
         if (remainingBreakTime > 0) {
           remainingBreakTime--;
           const minutes = Math.floor(remainingBreakTime / 60);
           const seconds = remainingBreakTime % 60;
 
           // Atualiza o tempo exibido durante o descanso
-          this.displayTime = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
+          this.breakDisplayTime = `${this.padZero(minutes)}:${this.padZero(seconds)}`;
         } else {
           this.stopBreakTimer();
         }
@@ -129,14 +104,12 @@ export class RepetitiveExercisePage implements OnInit {
     }
   }
 
-  // Para o timer de descanso
   stopBreakTimer() {
     this.breakTimerActive = false;
-    clearInterval(this.breakInterval);
-    this.startTimer();
-    this.breakTimerColor = this.originalTimerColor; // Retorna ao timer principal após o descanso
+    clearInterval(this.breakTimerInterval);
+    this.breakDisplayTime = `${this.padZero(Math.floor(this.breakTimeInSeconds / 60))}:${this.padZero(this.breakTimeInSeconds % 60)}`;
+    this.breakTimerColor = this.originalTimerColor;
   }
-
 
   incrementRepetition() {
     if (this.isRunning) {
@@ -146,17 +119,32 @@ export class RepetitiveExercisePage implements OnInit {
 
   async completeExercise() {
     if (!this.userId || !this.exerciseData) {
-      console.error('Dados d  e usuário ou exercício ausentes.');
+      console.error('Dados de usuário ou exercício ausentes.');
       return;
     }
 
     try {
       await this.exerciseService.completeExercise(this.userId, this.exerciseData);
 
+      // Incrementa o índice da série atual
+      this.exerciseData.currentSerieIndex = (this.exerciseData.currentSerieIndex || 0) + 1;
+
+      // Verifica se todas as séries foram concluídas
+      if (this.exerciseData.currentSerieIndex >= this.exerciseData.exercises.length) {
+        console.log('Exercício completo! Avançando para o próximo...');
+        // Reinicia o índice da série
+        this.exerciseData.currentSerieIndex = 0;
+        // Adiciona lógica para avançar para o próximo exercício ou concluir o treino
+        // ... (adicione sua lógica aqui)
+
+      } else {
+        // Se ainda houver séries restantes, reinicia o timer para a próxima série
+        this.stopTimer();
+      }
+
       console.log('Treino completo adicionado à coleção completedExercises!');
     } catch (error) {
       console.error('Erro ao adicionar treino completo:', error);
     }
   }
-
 }
